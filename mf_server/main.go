f073a5c2d5c54cfde022c3408f67dbc2ff7928d9
Nielsen Mike tbnielse <mike.nielsen@stud.hslu.ch>
2021-08-19 17:17:00 +0200

    Untar File
    - make sure the folder exists

--- mf_server/main.go
+++ mf_server/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"archive/tar"
 	"fmt"
-	"github.com/mholt/archiver/v3"
+	"io"
 	"os"
+	"path/filepath"
 )
 
 func main() {
@@ -11,7 +13,66 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 	}
-	err = archiver.Unarchive(workingDir + "/wep-app-node.tar", workingDir + "/wep-app-node")
+	var file, _ = os.Open(workingDir + "/wep-app-node.tar.gz")
+	err = Untar(workingDir + "/wep-app-node", file)
 	fmt.Println(err)
 }
 
+func Untar(dst string, r io.Reader) error {
+
+	tr := tar.NewReader(r)
+
+	for {
+		header, err := tr.Next()
+
+		switch {
+
+		// if no more files are found return
+		case err == io.EOF:
+			return nil
+
+		// return any other error
+		case err != nil:
+			return err
+
+		// if the header is nil, just skip it (not sure how this happens)
+		case header == nil:
+			continue
+		}
+
+		// the target location where the dir/file should be created
+		target := filepath.Join(dst, header.Name)
+
+		// the following switch could also be done using fi.Mode(), not sure if there
+		// a benefit of using one vs. the other.
+		// fi := header.FileInfo()
+
+		// check the file type
+		switch header.Typeflag {
+
+		// if its a dir and it doesn't exist create it
+		case tar.TypeDir:
+			if _, err := os.Stat(target); err != nil {
+				if err := os.MkdirAll(target, 0755); err != nil {
+					return err
+				}
+			}
+
+		// if it's a file create it
+		case tar.TypeReg:
+			f, err := os.OpenFile(target, os.O_CREATE|os.O_RDWR, os.FileMode(header.Mode))
+			if err != nil {
+				return err
+			}
+
+			// copy over contents
+			if _, err := io.Copy(f, tr); err != nil {
+				return err
+			}
+
+			// manually close here after each file operation; defering would cause each file close
+			// to wait until all operations have completed.
+			f.Close()
+		}
+	}
+}
