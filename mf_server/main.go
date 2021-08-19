83dc20d15652278e043aba1827c65157bec2fbc1
Nielsen Mike tbnielse <mike.nielsen@stud.hslu.ch>
2021-08-19 16:54:23 +0200

    Untar files

--- mf_server/main.go
+++ mf_server/main.go
@@ -1,6 +1,17 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+	"github.com/mholt/archiver/v3"
+	"os"
+)
 
 func main() {
-	fmt.Println("hello world")
+	workingDir, err := os.Getwd()
+	if err != nil {
+		fmt.Println(err)
+	}
+	err = archiver.Unarchive(workingDir + "/wep-app-node.tar", workingDir + "/wep-app-node")
+	fmt.Println(err)
 }
+
