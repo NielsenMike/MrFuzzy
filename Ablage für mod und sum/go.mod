5bc1833122f2a5f7bd20a88758508e6cec75e875
Opolor <56446631+Opolor@users.noreply.github.com>
2021-09-04 12:21:28 +0200

    Lenn - Update

---
+++ Ablage für mod und sum/go.mod
@@ -0,0 +1,5 @@
+module mf_server/main
+
+go 1.16
+
+require github.com/glaslos/ssdeep v0.3.1
--- mf_server/main.go
+++ mf_server/main.go
@@ -4,9 +4,11 @@ import (
 	"archive/tar"
 	"container/list"
 	"crypto/sha256"
+	"database/sql"
 	"encoding/hex"
 	"fmt"
 	"github.com/glaslos/ssdeep"
+	_ "github.com/mattn/go-sqlite3"
 	"io"
 	"log"
 	"os"
@@ -27,6 +29,8 @@ func main() {
 	if err != nil{
 		fmt.Println(err) // something went wrong
 	}
+
+	/*
 	// output of all file hashing data of tar file
 	for f := data.Front(); f != nil; f = f.Next() {
 		fmt.Println(f.Value.(fileHashingData).Name +
@@ -35,6 +39,14 @@ func main() {
 			" " + f.Value.(fileHashingData).ssdeepHash,
 		)
 	}
+	*/
+
+	//Store Data in SQLite Format
+	writeDatabase(data)
+
+	//Read Database
+	readDatabase()
+
 }
 
 // file hashing data
@@ -97,5 +109,95 @@ func readFiles(header *tar.Header, data []byte) fileHashingData{
 	}
 	// return file hashing data element
 	return fileData
+}
+
+//Write Hashing Data into SQLLite Database --> READ TODOS
+func writeDatabase(data *list.List){
+
+	//Creates a db file with table hashed
+	fmt.Println("Creating database")
+	file, err := os.Create("./hashing.db")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	_ = file.Close()
+	fmt.Println("Created database")
+
+	database, err := sql.Open("sqlite3", "./hashing.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer database.Close()
+
+	//CreateTable Database --> this could be an if statement so we don't always create a new table
+	createTable(database)
+
+	//WriteTable Database
+	writeTable(database, data)
+
+}
+
+
+//create Table function
+func createTable(database *sql.DB){
+	createHashingTable := "CREATE TABLE IF NOT EXISTS hashed (" +
+		"id INTEGER PRIMARY KEY, " +
+		"name TEXT, " +
+		"size TEXT, " +
+		"sha256Hash TEXT, " +
+		"ssdeepHash TEXT);"
+	fmt.Println("Making Table")
+	statement, err := database.Prepare(createHashingTable)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	statement.Exec()
+	fmt.Println("Made Table")
+}
+
+func writeTable(database *sql.DB, data *list.List){
+
+	fmt.Println("Writing to Database...")
+	insertData := "INSERT INTO hashed(name, size, sha256Hash, ssdeepHash) VALUES (?,?,?,?)"
+	statement, err := database.Prepare(insertData)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	//For looping through data and adding values
+	for f := data.Front(); f != nil; f = f.Next() {
+		statement.Exec(f.Value.(fileHashingData).Name, f.Value.(fileHashingData).Size,
+			f.Value.(fileHashingData).sha256Hash, f.Value.(fileHashingData).ssdeepHash)
+	}
+	fmt.Println("Write Complete")
+}
+
+//Query Database for Hashing Data
+func readDatabase(){
+	database, err := sql.Open("sqlite3", "./hashing.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer database.Close()
+
+	//Reading
+	fmt.Println("Reading")
+	row, err := database.Query("SELECT * FROM hashed")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	defer row.Close()
+
+	for row.Next(){
+		var id int
+		var name string
+		var size string
+		var sha256Hash string
+		var ssdeepHash string
+		row.Scan(&id, &name, &size, &sha256Hash, &ssdeepHash)
+		fmt.Println(strconv.Itoa(id) + ": " + name + ": " + size + ": " + sha256Hash + ": " + ssdeepHash)
+		fmt.Println("Read Complete")
+	}
+
+
 }
 
