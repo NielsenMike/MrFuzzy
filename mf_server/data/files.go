716886089e1bfca65abc0812b317d68a121a0101
Nielsen Mike tbnielse <mike.nielsen@stud.hslu.ch>
2021-09-28 09:25:52 +0200

    Refactoring
    - Changed some code

--- mf_server/data/files.go
+++ mf_server/data/files.go
@@ -1,21 +1,59 @@
 package Data
 
 import (
+	"errors"
+	"fmt"
 	"io/fs"
+	"os"
 	"path/filepath"
+	"strings"
 )
 
+func CreateFile(absolutePath string) *os.File{
+	file, err := os.Create(absolutePath)
+	if err != nil {
+		fmt.Println("Error creating file: \n"+err.Error())
+	}
+	_ = file.Close()
+	return file
+}
+
+// Check if file exists
+func FileExists(absolutePath string) bool {
+	_, err := os.Stat(absolutePath)
+	if err == nil {
+		return true
+	}
+	if errors.Is(err, os.ErrNotExist) {
+		return false
+	}
+	return false
+}
+
 // Find files with a extension and returns absolut path
-func Find(root, ext string) []string {
+func FindFilesByExtension(rootPath, ext string) []string {
 	var files []string
-	filepath.WalkDir(root, func(s string, d fs.DirEntry, e error) error {
+	filepath.WalkDir(rootPath, func(s string, d fs.DirEntry, e error) error {
 		if e != nil { return e }
 		if filepath.Ext(d.Name()) == ext {
-			files = append(files, root + "/" +d.Name())
+			files = append(files, rootPath + "/" +d.Name())
 		}
 		return nil
 	})
 	return files
+}
+
+func GetPathFromString(absolutePath string) string{
+	var paths  = strings.Split(absolutePath, "/")
+	paths = paths[:len(paths)-1]
+	var finalpath = strings.Join(paths, "/")
+	return finalpath
+}
+
+func GetFilenameFromString(absoulutePath string) string{
+	var pathname  = strings.Split(absoulutePath, "/")
+	var filename = pathname[len(pathname)-1]
+	return filename
 }
 
 func ReplaceExt(filename, repExt string) string {
--- mf_server/main.go
+++ mf_server/main.go
@@ -18,19 +18,20 @@ func main() {
 	// check if both arguments are set
 	if *achriveDirPtr != "" {
 		// open tar files
-		for _, filename := range Data.Find(*achriveDirPtr, ".tar") {
+		for _, filename := range Data.FindFilesByExtension(*achriveDirPtr, ".tar") {
 			tarReaderPtr := tarreader.Open(filename)
 			data := list.New()
 			err := tarreader.Read(tarReaderPtr, data) // read tar file and return list of file hashing data
 			if err != nil {
 				fmt.Println("Reading .tar file failed - ERROR: " + err.Error())
 			}
-			dbFilename := Data.ReplaceExt(filename, "db")
-			//Store Data in SQLite Format
-			sqlite.WriteDatabase(dbFilename, data)
+			dbAbsolutePath := Data.ReplaceExt(filename, "db")
+			var dbPtr = sqlite.OpenDatabase(dbAbsolutePath)
+			if dbPtr != nil{
+				sqlite.WriteDataIntoDatabase(dbPtr, data)
+			}
 		}
 	}
-	sqlite.Cleanup(*achriveDirPtr)
 }
 
 
