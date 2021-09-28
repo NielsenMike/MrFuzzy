f5fabcd8ce179ac023f7266cdbde6edd93f8169c
Opolor <56446631+Opolor@users.noreply.github.com>
2021-09-28 15:59:41 +0200

    Shell Scripts on RaspberryPi Updated

---
+++ client/exportToServer.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#Find & Send  Files
+FILES="/home/mf_exporter/tarfiles/*"
+now=$(date)
+
+echo "$now : Script Started" >> /home/mf_exporter/logs/logs.txt
+
+if [[ -z "$(ls -A /home/mf_exporter/tarfiles/)" ]]; then
+        echo "$now : Tarfiles Directory Empty, Ending..." >> /home/mf_exporter/logs/logs.txt
+else
+        echo "not empty"
+        for tars in $FILES
+        do
+                echo "Sending $tars file"
+                scp $tars mf_exporter@wiproh21-mnielsen.el.eee.intern:/home/mf_exporter/raspberry1
+                if [[ $? != 0 ]]; then
+                        echo "$now : scp $? : Error sending files" >> home/mf_exporter/logs/logs.txt
+                fi 
+                echo "File Sent!"
+done
+fi
+
+echo "$now : Script Finished " >> /home/mf_exporter/logs/logs.txt
+echo " " >> /home/mf_exporter/logs/logs.txt
